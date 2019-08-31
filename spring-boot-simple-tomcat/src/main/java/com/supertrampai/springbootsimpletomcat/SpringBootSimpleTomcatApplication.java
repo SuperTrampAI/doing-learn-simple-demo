e83692c326746683629f98587efa9a28df693b50
SuperTrampAI <lxh800109@gmail.com>
2019-08-31 14:43:38 +0800

    spring tomcat
     spring tomcat

---
+++ spring-boot-simple-tomcat/src/main/java/com/supertrampai/springbootsimpletomcat/SpringBootSimpleTomcatApplication.java
@@ -0,0 +1,13 @@
+package com.supertrampai.springbootsimpletomcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSimpleTomcatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSimpleTomcatApplication.class, args);
+    }
+
+}
