c09c2248b1e3e2a85d23024ed999718011da5422
SuperTrampAI <lxh800109@gmail.com>
2019-09-26 21:14:03 +0800

    init bio-nio-aio
    init bio-nio-aio

---
+++ bio-nio-aio-simple-learn/src/main/java/com/supertrampai/bionioaiosimplelearn/BioNioAioSimpleLearnApplication.java
@@ -0,0 +1,13 @@
+package com.supertrampai.bionioaiosimplelearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BioNioAioSimpleLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BioNioAioSimpleLearnApplication.class, args);
+    }
+
+}
